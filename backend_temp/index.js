ce8e79e49f8187b56d870a886c9ba291c16e64fe
Alexander <aleksandrs.goncaruks@gmail.com>
2017-10-31 21:58:48 +0200

    feat(endpoints, survey): add comments for endpoints + change question subtitle styles

--- backend_temp/index.js
+++ backend_temp/index.js
@@ -3,9 +3,23 @@ var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
 var handle = {};
+/**
+ * GET
+ */
 handle["/get/schools"] = requestHandlers.getSchools;
+/**
+ * GET
+ */
 handle["/get/questions"] = requestHandlers.getQuestions;
+/**
+ * POST
+ * Params: {student, answers}
+ */
 handle["/save/answers"] = requestHandlers.saveAnswers;
+/**
+ * POST
+ * Params: {contentId}
+ */
 handle["/get/study/content"] = requestHandlers.getStudyContent;
 
 server.start(router.route, handle);
